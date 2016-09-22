d2ac4952ac52ae0727ec151913b3e58e12981e03
itoshun <tyuke.net@gmail.com>
2016-09-22 12:45:03 +0900

    add prepare

--- ProfilesSample/SecondViewController.swift
+++ ProfilesSample/SecondViewController.swift
@@ -9,11 +9,17 @@
 import UIKit
 
 class SecondViewController: UIViewController {
+    
+    
+    @IBOutlet weak var LabelName: UILabel!
+    @IBOutlet weak var LabelPercent: UILabel!
+    
+    var strName :String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setLabelText()
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,14 +28,8 @@ class SecondViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    private func setLabelText(){
+        LabelName.text = strName
     }
-    */
-
+    
 }
